9eee76070e4a5c4ac28bd137e52e232cb1125143
IsaSuzumura <isabelasuzumura@gmail.com>
2025-06-22 23:55:12 -0300

    atualização

--- index.js
+++ index.js
@@ -3,10 +3,10 @@ require('dotenv').config();
 const express = require('express');
 const mongoose = require('mongoose');
 const produtoRoutes = require('./routes/produtoRoutes');
-
+const { swaggerUi, swaggerSpec } = require('./swagger');
 const app = express();
 app.use(express.json());
-
+app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerSpec));
 app.use('/produtos', produtoRoutes);
 
 mongoose.connect(process.env.MONGODB_URI)
@@ -15,4 +15,5 @@ mongoose.connect(process.env.MONGODB_URI)
     app.listen(3000, () => console.log('Servidor rodando na porta 3000'));
   })
   .catch((err) => console.log(err));
+
 
